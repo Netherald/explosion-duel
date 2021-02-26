cabb37bacbfb0e12b1408fd9d69f5e1ffbbbb0fe
명이 <76814120+myoung2namur@users.noreply.github.com>
2021-02-26 09:55:20 +0900

    Delete src/main/java directory

--- src/main/java/com/github/highright1234/duel/InviteHashMap.java
+++
@@ -1,9 +0,0 @@
-package com.github.highright1234.duel;
-
-import org.bukkit.entity.Player;
-
-import java.util.HashMap;
-
-public class InviteHashMap {
-    public static HashMap<Player, Player> sendHashMap = new HashMap<>();
-}
