358f354619efe19df2b94f9a4457ecf317e1f508
TreeKR <johanbag290@gmail.com>
2021-02-28 11:03:19 +0900

    중간저장

--- src/main/kotlin/com/github/netherald/duel/listeners/TntThrowListener.kt
+++ src/main/kotlin/com/github/netherald/duel/listeners/TntThrowListener.kt
@@ -1,5 +1,7 @@
 package com.github.netherald.duel.listeners
 
+import net.minecraft.server.v1_8_R3.PacketPlayOutChat
+import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.entity.Fireball
@@ -12,10 +14,17 @@ import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.util.Vector
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer
+
+import net.minecraft.server.v1_8_R3.ChatComponentText
+
+data class CoolTime(var now: Float, val max: Int, var percent: Float)
 
 class TntThrowListener : Listener {
 
     val dangerIds = ArrayList<UUID>()
+    var cooltimesTnt = HashMap<UUID, CoolTime>()
 
     @EventHandler
     fun onPlayerRightClickTnt(e:PlayerInteractEvent) {
@@ -28,12 +37,45 @@ class TntThrowListener : Listener {
         if (item.itemMeta.displayName == tlName) {
 
             if (action == Action.RIGHT_CLICK_AIR && item.type == Material.TNT) {
-                val v = 2
-                val direction:Vector = player.eyeLocation.direction.multiply(v)
-                val tnt = player.world.spawn(player.eyeLocation.add(direction),TNTPrimed::class.java)
+                if(!cooltimesTnt.containsKey(e.player.uniqueId)) {
+                    val v = 2
+                    val direction: Vector = player.eyeLocation.direction.multiply(v)
+                    val tnt = player.world.spawn(player.eyeLocation.add(direction), TNTPrimed::class.java)
 
-                tnt.velocity = direction
-                player.sendMessage("발사!")
+                    tnt.fuseTicks = 20
+                    tnt.velocity = direction
+                    player.sendMessage("발사!")
+                    cooltimesTnt[e.player.uniqueId] = CoolTime(5.0F, 5, 100F)
+
+                    val onePercent = (100 / cooltimesTnt[e.player.uniqueId]?.max!!) / 20
+                    println(onePercent)
+
+                    val scheduleId =
+                        Bukkit.getScheduler().scheduleSyncRepeatingTask(Bukkit.getPluginManager().getPlugin("duel"), {
+
+                            var ps = "["
+                            println(Math.round(cooltimesTnt[e.player.uniqueId]?.percent!!))
+                            for (i in 1..Math.round(cooltimesTnt[e.player.uniqueId]?.percent!!)) {
+                                ps += "|"
+                            }
+                            ps += "]"
+
+                            val packet = PacketPlayOutChat(
+                                ChatComponentText("${ChatColor.RED}쿨타임: ${cooltimesTnt[e.player.uniqueId]?.now} $ps"),
+                                2.toByte()
+                            )
+                            (player as CraftPlayer).handle.playerConnection.sendPacket(packet)
+                            cooltimesTnt[e.player.uniqueId]?.now = cooltimesTnt[e.player.uniqueId]?.now?.minus(0.1F)!!
+                            cooltimesTnt[e.player.uniqueId]?.percent = cooltimesTnt[e.player.uniqueId]?.percent?.minus(onePercent)!!
+                        }, 0L, 2L)
+
+                    Bukkit.getScheduler().scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("duel"), {
+                        val packet = PacketPlayOutChat(ChatComponentText("${ChatColor.RED}쿨타임: 0"), 2.toByte())
+                        (player as CraftPlayer).handle.playerConnection.sendPacket(packet)
+                        cooltimesTnt.remove(player.uniqueId)
+                        Bukkit.getScheduler().cancelTask(scheduleId)
+                    }, (cooltimesTnt[e.player.uniqueId]?.max?.times(20))!!.toLong())
+                }
 
             }
             else if (action == Action.RIGHT_CLICK_BLOCK && item.type == Material.TNT) {
