dc447ad3dae29b13557438b9ff83deff712b4ea3
TreeKR <johanbag290@gmail.com>
2021-02-28 12:15:57 +0900

    중간저장

--- src/main/kotlin/com/github/netherald/duel/listeners/TntThrowListener.kt
+++ src/main/kotlin/com/github/netherald/duel/listeners/TntThrowListener.kt
@@ -18,6 +18,9 @@ import kotlin.collections.HashMap
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer
 
 import net.minecraft.server.v1_8_R3.ChatComponentText
+import kotlin.math.floor
+import kotlin.math.round
+import kotlin.math.roundToInt
 
 data class CoolTime(var now: Float, val max: Int, var percent: Float)
 
@@ -25,6 +28,19 @@ class TntThrowListener : Listener {
 
     val dangerIds = ArrayList<UUID>()
     var cooltimesTnt = HashMap<UUID, CoolTime>()
+    var cooltimesNuclear = HashMap<UUID, CoolTime>()
+
+    fun getPsNum(now: Float, max: Int) : String {
+        var psNum = "["
+        for(i in 1..(now * 4).roundToInt()) {
+            psNum += "■"
+        }
+        for(i in 1..((max * 4) - (now * 4).roundToInt())) {
+            psNum += "□"
+        }
+        psNum += "]"
+        return psNum
+    }
 
     @EventHandler
     fun onPlayerRightClickTnt(e:PlayerInteractEvent) {
@@ -47,31 +63,38 @@ class TntThrowListener : Listener {
                     player.sendMessage("발사!")
                     cooltimesTnt[e.player.uniqueId] = CoolTime(5.0F, 5, 100F)
 
-                    val onePercent = (100 / cooltimesTnt[e.player.uniqueId]?.max!!) / 20
-                    println(onePercent)
-
                     val scheduleId =
                         Bukkit.getScheduler().scheduleSyncRepeatingTask(Bukkit.getPluginManager().getPlugin("duel"), {
-
-                            var ps = "["
-                            println(Math.round(cooltimesTnt[e.player.uniqueId]?.percent!!))
-                            for (i in 1..Math.round(cooltimesTnt[e.player.uniqueId]?.percent!!)) {
-                                ps += "|"
+                            if(player.inventory.itemInHand.type == Material.TNT) {
+                                val packet = PacketPlayOutChat(
+                                    ChatComponentText(
+                                        "${ChatColor.RED}TNT - ${
+                                            String.format(
+                                                "%.1f",
+                                                cooltimesTnt[e.player.uniqueId]?.now!!
+                                            )
+                                        }s - ${
+                                            getPsNum(
+                                                cooltimesTnt[e.player.uniqueId]?.now!!,
+                                                cooltimesTnt[e.player.uniqueId]?.max!!
+                                            )
+                                        }"
+                                    ),
+                                    2.toByte()
+                                )
+                                (player as CraftPlayer).handle.playerConnection.sendPacket(packet)
                             }
-                            ps += "]"
-
-                            val packet = PacketPlayOutChat(
-                                ChatComponentText("${ChatColor.RED}쿨타임: ${cooltimesTnt[e.player.uniqueId]?.now} $ps"),
-                                2.toByte()
-                            )
-                            (player as CraftPlayer).handle.playerConnection.sendPacket(packet)
                             cooltimesTnt[e.player.uniqueId]?.now = cooltimesTnt[e.player.uniqueId]?.now?.minus(0.1F)!!
-                            cooltimesTnt[e.player.uniqueId]?.percent = cooltimesTnt[e.player.uniqueId]?.percent?.minus(onePercent)!!
                         }, 0L, 2L)
 
                     Bukkit.getScheduler().scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("duel"), {
-                        val packet = PacketPlayOutChat(ChatComponentText("${ChatColor.RED}쿨타임: 0"), 2.toByte())
-                        (player as CraftPlayer).handle.playerConnection.sendPacket(packet)
+                        if (player.inventory.itemInHand.type == Material.TNT) {
+                            val packet = PacketPlayOutChat(
+                                ChatComponentText("${ChatColor.RED}TNT - 0.0s - [□□□□□□□□□□□□□□□□□□□□]"),
+                                2.toByte()
+                            )
+                            (player as CraftPlayer).handle.playerConnection.sendPacket(packet)
+                        }
                         cooltimesTnt.remove(player.uniqueId)
                         Bukkit.getScheduler().cancelTask(scheduleId)
                     }, (cooltimesTnt[e.player.uniqueId]?.max?.times(20))!!.toLong())
@@ -87,13 +110,51 @@ class TntThrowListener : Listener {
         else if (item.itemMeta.displayName == nlName) {
 
             if (action == Action.RIGHT_CLICK_AIR && item.type == Material.FIREBALL) {
-                val v = 2
-                val direction:Vector = player.eyeLocation.direction.multiply(v)
-                val ball = player.world.spawn(player.eyeLocation.add(direction),Fireball::class.java)
-                dangerIds.add(ball.uniqueId)
-                ball.velocity = direction
-                player.sendMessage("발사!")
+                if(!cooltimesNuclear.containsKey(e.player.uniqueId)) {
+                    val v = 2
+                    val direction: Vector = player.eyeLocation.direction.multiply(v)
+                    val ball = player.world.spawn(player.eyeLocation.add(direction), Fireball::class.java)
+                    dangerIds.add(ball.uniqueId)
+                    ball.velocity = direction
+                    player.sendMessage("발사!")
+                    cooltimesNuclear[e.player.uniqueId] = CoolTime(5.0F, 5, 100F)
+                    val scheduleId =
+                        Bukkit.getScheduler().scheduleSyncRepeatingTask(Bukkit.getPluginManager().getPlugin("duel"), {
+                            if (player.inventory.itemInHand.type == Material.FIREBALL) {
+                                val packet = PacketPlayOutChat(
+                                    ChatComponentText(
+                                        "${ChatColor.RED}핵폭탄 - ${
+                                            String.format(
+                                                "%.1f",
+                                                cooltimesNuclear[e.player.uniqueId]?.now!!
+                                            )
+                                        }s - ${
+                                            getPsNum(
+                                                cooltimesNuclear[e.player.uniqueId]?.now!!,
+                                                cooltimesNuclear[e.player.uniqueId]?.max!!
+                                            )
+                                        }"
+                                    ),
+                                    2.toByte()
+                                )
+                                (player as CraftPlayer).handle.playerConnection.sendPacket(packet)
+                            }
 
+                            cooltimesNuclear[e.player.uniqueId]?.now = cooltimesNuclear[e.player.uniqueId]?.now?.minus(0.1F)!!
+                        }, 0L, 2L)
+
+                    Bukkit.getScheduler().scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("duel"), {
+                        if (player.inventory.itemInHand.type == Material.FIREBALL) {
+                            val packet = PacketPlayOutChat(
+                                ChatComponentText("${ChatColor.RED}핵폭탄 - 0.0s - [□□□□□□□□□□□□□□□□□□□□]"),
+                                2.toByte()
+                            )
+                            (player as CraftPlayer).handle.playerConnection.sendPacket(packet)
+                        }
+                        cooltimesNuclear.remove(player.uniqueId)
+                        Bukkit.getScheduler().cancelTask(scheduleId)
+                    }, (cooltimesNuclear[e.player.uniqueId]?.max?.times(20))!!.toLong())
+                }
             }
             else if (action == Action.RIGHT_CLICK_BLOCK && item.type == Material.FIREBALL) {
 
@@ -115,4 +176,10 @@ class TntThrowListener : Listener {
         }
     }
 
+}
+
+fun Double.round(decimals: Int): Double {
+    var multiplier = 1.0
+    repeat(decimals) { multiplier *= 10 }
+    return round(this * multiplier) / multiplier
 }
